031a4cb5bac95df8e65a7c5efd8df5232b0594ed
Maria Artymovych <mashkaart94@gmail.com>
2021-11-24 20:10:34 +0200

    refactor: added chapter numbers to the filenames

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-mod defining_structs;
-mod programming_a_guessing_game;
-mod rectangle_area;
+mod ch2_programming_a_guessing_game;
+mod ch5_1_defining_structs;
+mod ch5_2_rectangle_area;
 
-use crate::defining_structs::users_struct;
-use crate::programming_a_guessing_game::secret_number;
-use crate::rectangle_area::rect_area;
+use crate::ch2_programming_a_guessing_game::secret_number;
+use crate::ch5_1_defining_structs::users_struct;
+use crate::ch5_2_rectangle_area::rect_area;
 fn main() {
     //secret_number();
     //users_struct();
